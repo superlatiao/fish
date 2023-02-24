0ed42e3350f32e5e2721cf637a930c6b370b7785
zx <1592473423@qq.com>
2023-02-24 14:40:14 +0800

    first

---
+++ wx/pages/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="container">
+		
+		{{hello}}
+	</view>
+</template>
+
+<script setup>
+import{ ref } from "vue";
+	const hello = ref("hello world")
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
